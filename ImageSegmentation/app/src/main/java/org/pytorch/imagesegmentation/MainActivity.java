d98649cb998aafb4ad86b9227b2308b21738d2ff
Shivam Shrirao <shivamshrirao@gmail.com>
2022-03-27 06:26:55 +0530

    traced and testing app

---
+++ ImageSegmentation/app/src/main/java/org/pytorch/imagesegmentation/MainActivity.java
@@ -0,0 +1,153 @@
+package org.pytorch.imagesegmentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import org.pytorch.IValue;
+import org.pytorch.LiteModuleLoader;
+import org.pytorch.Module;
+import org.pytorch.Tensor;
+import org.pytorch.torchvision.TensorImageUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity implements Runnable {
+    private ImageView mImageView;
+    private Button mButtonSegment;
+    private ProgressBar mProgressBar;
+    private Bitmap mBitmap = null;
+    private Module mModule = null;
+    private String mImagename = "car.jpg";
+
+    // see http://host.robots.ox.ac.uk:8080/pascal/VOC/voc2007/segexamples/index.html for the list of classes with indexes
+    private static final int CLASSNUM = 1;
+
+    public static String assetFilePath(Context context, String assetName) throws IOException {
+        File file = new File(context.getFilesDir(), assetName);
+        if (file.exists() && file.length() > 0) {
+            return file.getAbsolutePath();
+        }
+
+        try (InputStream is = context.getAssets().open(assetName)) {
+            try (OutputStream os = new FileOutputStream(file)) {
+                byte[] buffer = new byte[4 * 1024];
+                int read;
+                while ((read = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, read);
+                }
+                os.flush();
+            }
+            return file.getAbsolutePath();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            mBitmap = BitmapFactory.decodeStream(getAssets().open(mImagename));
+        } catch (IOException e) {
+            Log.e("ImageSegmentation", "Error reading assets", e);
+            finish();
+        }
+
+        mImageView = findViewById(R.id.imageView);
+        mImageView.setImageBitmap(mBitmap);
+
+        final Button buttonRestart = findViewById(R.id.restartButton);
+        buttonRestart.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (mImagename == "car.jpg")
+                    mImagename = "car2.jpg";
+                else
+                    mImagename = "car.jpg";
+                try {
+                    mBitmap = BitmapFactory.decodeStream(getAssets().open(mImagename));
+                    mImageView.setImageBitmap(mBitmap);
+                } catch (IOException e) {
+                    Log.e("ImageSegmentation", "Error reading assets", e);
+                    finish();
+                }
+            }
+        });
+
+
+        mButtonSegment = findViewById(R.id.segmentButton);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mButtonSegment.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mButtonSegment.setEnabled(false);
+                mProgressBar.setVisibility(ProgressBar.VISIBLE);
+                mButtonSegment.setText(getString(R.string.run_model));
+
+                Thread thread = new Thread(MainActivity.this);
+                thread.start();
+            }
+        });
+
+        try {
+            mModule = LiteModuleLoader.load(MainActivity.assetFilePath(getApplicationContext(), "u2net_quantized.ptl"));
+        } catch (IOException e) {
+            Log.e("ImageSegmentation", "Error reading assets", e);
+            finish();
+        }
+    }
+
+    @Override
+    public void run() {
+        float[] mean = {0.0f, 0.0f, 0.0f};
+        float[] std = {1.0f, 1.0f, 1.0f};
+        final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(mBitmap, mean, std);
+//        final float[] inputs = inputTensor.getDataAsFloatArray();
+
+        final long startTime = SystemClock.elapsedRealtime();
+        Map<String, IValue> outTensors = mModule.forward(IValue.from(inputTensor)).toDictStringKey();
+        final long inferenceTime = SystemClock.elapsedRealtime() - startTime;
+        Log.d("ImageSegmentation",  "inference time (ms): " + inferenceTime);
+
+        final Tensor outputTensor = outTensors.get("out").toTensor();
+        final float[] scores = outputTensor.getDataAsFloatArray();
+        int width = mBitmap.getWidth();
+        int height = mBitmap.getHeight();
+        int[] intValues = new int[width * height];
+        for (int j = 0; j < height; j++) {
+            for (int k = 0; k < width; k++) {
+                intValues[j * width + k] = 0xFF000000 + Math.round(scores[j * width + k]);
+            }
+        }
+
+        Bitmap bmpSegmentation = Bitmap.createScaledBitmap(mBitmap, width, height, true);
+        Bitmap outputBitmap = bmpSegmentation.copy(bmpSegmentation.getConfig(), true);
+        outputBitmap.setPixels(intValues, 0, outputBitmap.getWidth(), 0, 0, outputBitmap.getWidth(), outputBitmap.getHeight());
+        final Bitmap transferredBitmap = Bitmap.createScaledBitmap(outputBitmap, mBitmap.getWidth(), mBitmap.getHeight(), true);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mImageView.setImageBitmap(transferredBitmap);
+                mButtonSegment.setEnabled(true);
+                mButtonSegment.setText(getString(R.string.segment));
+                mProgressBar.setVisibility(ProgressBar.INVISIBLE);
+
+            }
+        });
+    }
+}
